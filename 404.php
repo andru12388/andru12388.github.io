c9e9658208c342bea0f42ea702bb699f36de9790
andru12388 <67239247+andru12388@users.noreply.github.com>
2020-06-21 22:32:01 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,53 @@
+<?php
+Header("HTTP/1.1 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M42LZMJ');</script>
+	<!-- End Google Tag Manager -->
+	<title>404 – страница не найдена</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Template Basic Images Start -->
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="icon" href="img/favicon/favicon3.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/favicon2.png">
+	<!-- Template Basic Images End -->
+	
+	<!-- Custom Browsers Color Start -->
+	<meta name="theme-color" content="#000">
+	<!-- Custom Browsers Color End -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+	html, body {width:100%;height:100%;overflow:hidden;margin:0px;padding:0px;font-family:'Open Sans',sans-serif;font-size:16px}
+	body {background:url('img/404.png') center no-repeat #333039}
+	.content {width:100%;text-align:center;position:absolute;bottom:10%;left:0px;}
+	.content a {display:inline-block;text-decoration:none;font-size:20px}
+	.content a, .content a:hover {color:rgba(255,255,255,0.3);}
+	.content a:hover {color:rgba(255,255,255,0.5);}
+	@media only screen and (max-width: 460px), screen and (max-height: 700px) {
+		.content {position:static;}
+		.content a {display:block;width:100%;height:100%;position:absolute;top:0px;left:0px;font-size:0px;opacity:0;}
+		body {background-size:cover}
+	}
+</style>
+</head>
+<body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M42LZMJ"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<div class="content">
+		<a href="http://partnerpack.by">Перейти к главной странице</a>
+	</div>
+</body>
+</html>
